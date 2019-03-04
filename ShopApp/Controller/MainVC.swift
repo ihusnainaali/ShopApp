2f2801eaaf0532b2f5b26e011581017c025dce4a
Hesham Salama <theflash1970@gmail.com>
2019-03-04 23:38:08 +0200

    initial commit

---
+++ ShopApp/Controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopApp
+//
+//  Created by Hesham Salama on 3/4/19.
+//  Copyright © 2019 hesham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ShopApp/Controller/WelcomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  ShopApp
+//
+//  Created by Hesham Salama on 3/4/19.
+//  Copyright © 2019 hesham. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
